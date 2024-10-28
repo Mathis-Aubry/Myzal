71e07bd408ff988fcfc7def93265c6fc037a1ed6
Mathis <72527059+Mathis-Aubry@users.noreply.github.com>
2024-10-28 08:42:08 +0000

    better than better

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "♤", function()
+vim.keymap.set("n", "<M-l>", function()
   vim.o.bg = "light"
-end)
-vim.keymap.set("n", "♠", function()
+end, { remap = true })
+vim.keymap.set("n", "<M-d>", function()
   vim.o.bg = "dark"
-end)
+end, { remap = true })
