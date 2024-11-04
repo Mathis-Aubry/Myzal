6056e98d22be85835b040a67e6700790bf953cd5
mathis-aubry <72527059+Mathis-Aubry@users.noreply.github.com>
2024-11-04 16:54:40 +0100

    butter and shiny

--- lua/plugins/mine.lua
+++ lua/plugins/mine.lua
@@ -2,7 +2,7 @@ return {
   {
     "Pocco81/auto-save.nvim",
     config = function()
-    require("auto-save").setup()
+      require("auto-save").setup()
     end,
   },
   {
@@ -12,13 +12,27 @@ return {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "solarized",
-    }
+    },
   },
   {
     "akinsho/toggleterm.nvim",
     version = "*",
     opts = {
       shade_terminals = false,
+      shell = "(set -o emacs && bash)",
     },
+  },
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    ft = "markdown",
+    config = function()
+      vim.g.mkdp_auto_start = 1
+    end,
   },
 }
